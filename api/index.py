5853c1de2ce915bea02b12e31eafebc562aab2e5
Carlos André <techcarlosandre@gmail.com>
2026-05-06 21:00:34 -0300

    debug: restaurando middleware no servidor pena

--- api/index.py
+++ api/index.py
@@ -1,7 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
+
+class PrefixMiddleware(object):
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+        return self.app(environ, start_response)
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/_/backend')
 
 @app.route('/health')
 @app.route('/api/health')
